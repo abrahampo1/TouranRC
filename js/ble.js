f476fa86e98e5b2c156e56d2c490b4d953e3a317
Abraham Leiro Fernandez <abraham@cpsoftware.es>
2022-09-23 22:35:49 +0200

    sistema de musica mejorado

---
+++ js/ble.js
@@ -0,0 +1,82 @@
+function light(l) {
+  if (serv) {
+    l.classList.toggle("active");
+  }
+  send_command("lights_toggle");
+}
+function send_command(comm) {
+  if (serv) {
+    var enc = new TextEncoder();
+    serv.writeValue(enc.encode(comm));
+  } else {
+    onButtonClick();
+  }
+}
+let serv;
+function onButtonClick(btn) {
+  // Validate services UUID entered by user first.
+
+  navigator.bluetooth
+    .requestDevice({
+      filters: [{ name: "VWTOURAN" }],
+    })
+    .then((device) => {
+      return device.gatt.connect();
+    })
+    .then((server) => {
+      // Note that we could also get all services that match a specific UUID by
+      // passing it to getPrimaryServices().
+      return server.getPrimaryServices();
+    })
+    .then((services) => {
+      let queue = Promise.resolve();
+      services.forEach((service) => {
+        queue = queue.then((_) => {
+          service.getCharacteristic(0xffe1).then((characteristic) => {
+            serv = characteristic;
+            document.getElementById("connect").remove();
+          });
+          service.getCharacteristic("FFE1").then((characteristic) => {
+            serv = characteristic;
+            document.getElementById("connect").remove();
+          });
+        });
+      });
+      return queue;
+    })
+    .catch((error) => {
+      alert("Argh! " + error);
+    });
+}
+
+function flash(l) {
+  if (serv) {
+    l.classList.toggle("active");
+  }
+  send_command("lights_flash");
+  setTimeout(() => {
+    if (serv) {
+      l.classList.remove("active");
+    }
+  }, 200);
+}
+
+/* Utils */
+
+function getSupportedProperties(characteristic) {
+  let supportedProperties = [];
+  for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+      supportedProperties.push(p.toUpperCase());
+    }
+  }
+  return "[" + supportedProperties.join(", ") + "]";
+}
+
+function toHex(str) {
+  var result = "";
+  for (var i = 0; i < str.length; i++) {
+    result += str.charCodeAt(i).toString(16);
+  }
+  return result;
+}
--- js/musica.js
+++ js/musica.js
@@ -1,13 +1,50 @@
 let guernica_notes = [
-  6900, 7200, 7600, 7900, 8400, 8600, 8800, 8950, 9100, 9300, 9900, 10000,
+  7000, 7200, 7600, 7900, 8400, 8600, 8800, 8950, 9100, 9300, 9900, 10000,
   10600, 10900, 11300, 11500, 11750, 11900, 12150, 12400, 12800, 13300, 13600,
-  13800,
+  13800, 14300, 14500, 14700, 14900, 15100, 15300, 15800, 16600, 16900, 17400,
+  17600, 17800, 18100, 18300, 18700, 19500, 20200, 20300, 20600, 20700,
 ];
+let guern = false;
 function guernica(btn) {
+  if (!guern) {
+    guern = true;
+  } else {
+    return;
+  }
   document.getElementById("guernica").play();
-  guernica_notes.forEach((note) => {
+  document.getElementById("guernica").onplay = function () {
+    guernica_notes.forEach((note) => {
+      setTimeout(() => {
+        light(btn);
+      }, note);
+    });
     setTimeout(() => {
-      light(btn);
-    }, note);
-  });
+      btn.classList.remove("active");
+      guern = false;
+    }, 21000);
+  };
+}
+
+let lolita_notes = [
+  1300, 1800, 2100, 3100, 3600, 4400, 4600, 5000, 5200, 5500, 5900,
+];
+let lolita = false;
+function lolitalola(btn) {
+  if (!lolita) {
+    lolita = true;
+  } else {
+    return;
+  }
+  document.getElementById("lolitalola").play();
+  document.getElementById("lolitalola").onplay = function () {
+    lolita_notes.forEach((note) => {
+      setTimeout(() => {
+        light(btn);
+      }, note);
+    });
+    setTimeout(() => {
+      btn.classList.remove("active");
+      lolita = false;
+    }, 21000);
+  };
 }
